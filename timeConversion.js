5eb5d88324e79612938c855725c17948a58975e8
Ruan6400 <133367215+Ruan6400@users.noreply.github.com>
2024-03-07 09:44:50 -0300

    Add files via upload

---
+++ timeConversion.js
@@ -0,0 +1,20 @@
+function Horario(Hora){
+                let Hora24=Hora;
+                let novaHora = Hora[0]+Hora[1];
+                switch (Hora[8]){
+                    case "A":
+                        if(Hora[0]+Hora[1] == "12"){
+                            novaHora="00";
+                        }
+                        break;
+                    case "P":
+                        if(Hora[0]+Hora[1]!="12"){
+                            novaHora= (Hora[0]+Hora[1])-(-12);
+                        }
+                        break;
+                }
+                for(let i= 2;i< Hora.length-2;i++){
+                    novaHora+=Hora[i];
+                }
+                Hora24=novaHora;     
+}
