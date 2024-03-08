aea5956699581c5761212ca27b8ff9160d174eb2
Ruan6400 <133367215+Ruan6400@users.noreply.github.com>
2024-03-07 22:53:14 -0300

    Add files via upload

--- timeConversion.js
+++ timeConversion.js
@@ -1,21 +1,21 @@
 function Horario(Hora){
-                let Hora24=Hora;
-                let novaHora = Hora[0]+Hora[1];
-                switch (Hora[8]){
-                    case "A":
-                        if(Hora[0]+Hora[1] == "12"){
-                            novaHora="00";
-                        }
-                        break;
-                    case "P":
-                        if(Hora[0]+Hora[1]!="12"){
-                            novaHora= (Hora[0]+Hora[1])-(-12);
-                        }
-                        break;
-                }
-                for(let i= 2;i< Hora.length-2;i++){
-                    novaHora+=Hora[i];
-                }
-                Hora24=novaHora;
-                return Hora24;
+    let Hora24=Hora;
+    let novaHora = Hora[0]+Hora[1];
+    switch (Hora[8]){
+        case "A":
+            if(Hora[0]+Hora[1] == "12"){
+                novaHora="00";
+            }
+            break;
+        case "P":
+            if(Hora[0]+Hora[1]!="12"){
+                novaHora= (Hora[0]+Hora[1])-(-12);
+            }
+            break;
+    }
+    for(let i= 2;i< Hora.length-2;i++){
+        novaHora+=Hora[i];
+    }
+    Hora24=novaHora;
+    return Hora24;   
 }
